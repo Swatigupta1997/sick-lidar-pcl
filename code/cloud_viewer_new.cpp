4a70eb4ae66f6db51eabc9dd6678979d14ff64f8
Swati Gupta <gswati@seas.upenn.edu>
2023-04-08 10:25:23 -0400

    Point cloud visualisation with region segmentation

--- code/cloud_viewer_new.cpp
+++ code/cloud_viewer_new.cpp
@@ -1,112 +1,102 @@
-#include <pcl/visualization/cloud_viewer.h>
+
+/*
+* Author: Swati Gupta (gswati@seas.upenn.edu)
+* References: https://pcl.readthedocs.io/projects/tutorials/en/latest/region_growing_segmentation.html#region-growing-segmentation
+*             https://github.com/PointCloudLibrary/pcl/issues/1852
+*/
+
 #include <iostream>
-#include <pcl/io/io.h>
+#include <vector>
+#include <pcl/point_types.h>
 #include <pcl/io/pcd_io.h>
+#include <pcl/search/search.h>
+#include <pcl/search/kdtree.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/filters/filter_indices.h> // for pcl::removeNaNFromPointCloud
+#include <pcl/segmentation/region_growing.h>
 #include <string>
-    
-int user_data;
-int t = 1;
-    
-void 
-viewerOneOff (pcl::visualization::PCLVisualizer& viewer)
-{
-    viewer.setBackgroundColor (1.0, 0.5, 1.0);
-    pcl::PointXYZ o;
-    o.x = 1.0;
-    o.y = 0;
-    o.z = 0;
-    viewer.addSphere (o, 0.25, "sphere", 0);
-    std::cout << "i only run once" << std::endl;
-    
+#include <pcl/filters/passthrough.h>
+
+
+pcl::PointCloud <pcl::PointXYZRGB>::Ptr region_seg(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud) {
+  pcl::search::Search<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+  pcl::PointCloud <pcl::Normal>::Ptr normals (new pcl::PointCloud <pcl::Normal>);
+  pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normal_estimator;
+  normal_estimator.setSearchMethod (tree);
+  normal_estimator.setInputCloud (cloud);
+  normal_estimator.setKSearch (50);
+  normal_estimator.compute (*normals);
+
+  pcl::IndicesPtr indices (new std::vector <int>);
+  pcl::removeNaNFromPointCloud(*cloud, *indices);
+
+  pcl::RegionGrowing<pcl::PointXYZ, pcl::Normal> reg;
+  reg.setMinClusterSize (0);
+  reg.setMaxClusterSize (1000000);
+  reg.setSearchMethod (tree);
+  reg.setNumberOfNeighbours (30);
+  reg.setInputCloud (cloud);
+  reg.setIndices (indices);
+  reg.setInputNormals (normals);
+  reg.setSmoothnessThreshold (3.0 / 180.0 * M_PI);
+  reg.setCurvatureThreshold (1.0);
+
+  std::vector <pcl::PointIndices> clusters;
+  reg.extract (clusters);
+
+  std::cout << "Number of clusters is equal to " << clusters.size () << std::endl;
+
+  pcl::PointCloud <pcl::PointXYZRGB>::Ptr colored_cloud = reg.getColoredCloud ();
+  return colored_cloud;
 }
-    
-void 
-viewerPsycho (pcl::visualization::PCLVisualizer& viewer)
+
+// Can be modified to point to dataset dir
+std::string basedir = "/home/swati/Documents/SICK/PCL/repo/sick-lidar-pcl/code/data_collect_22Mar/";
+
+int main(int argc, const char **argv)
 {
-    // static unsigned count = 0;
-    // std::stringstream ss;
-    // ss << "Once per viewer loop: " << count++;
-    // viewer.removeShape ("text", 0);
-    // viewer.addText (ss.str(), 200, 300, "text", 0);
-    
-    // //FIXME: possible race condition here:
-    // user_data++;
-    if (t >=100) {
-        t=1;
-    } else {
-        t++;
-    }
+    pcl::visualization::PCLVisualizer viewer ("Matrix transformation example");
+    viewer.setBackgroundColor(0,0,0);
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::io::loadPCDFile ("data_collect_22Mar/record_"+ std::to_string(t)+".pcd", *cloud);
 
-    viewer.updatePointCloud(cloud, "cloud");
-    viewer.spinOnce();
-
-    cout << "t:" << t << endl;
+    if (argc != 3) {
+        std::cout << "Usage: ./filter_point_cloud <x-limit> <y-limit>" << std::endl;
+        return -1;
+    }
     
+    int t=1;
+    while(1)
+    {
+       // Clear the view
+       viewer.removeAllShapes();
+       viewer.removeAllPointClouds();
 
-    // viewer.spin();
-}
-    
-int 
-main (int argc, char* argv[])
-{
+       // Get the PCD file
+       pcl::io::loadPCDFile <pcl::PointXYZ> (basedir+"record_"+std::to_string(t)+".pcd", *cloud);
+       t++;
 
-    
-    
-    
-    //blocks until the cloud is actually rendered
-    
-    
-    //use the following functions to get access to the underlying more advanced/powerful
-    //PCLVisualizer
-    
-    //This will only get called once
-    // viewer.runOnVisualizationThreadOnce (viewerOneOff);
-    
-    //This will get called once per visualization iteration
- 
-    // int t=1;
-    // while (t <100)
-    // {
-    //     // pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-    //     // pcl::io::loadPCDFile ("filtered_data/record_"+ std::to_string(t)+".pcd", *cloud);
+        // Filter point cloud data to remove far away points/walls
+        pcl::PassThrough<pcl::PointXYZ> pass;
+        pass.setInputCloud(cloud);
+        pass.setFilterFieldName("x");
+        pass.setFilterLimits(-std::abs(std::stof(argv[1])), std::abs(std::stof(argv[1]))); // clamp points to the range of [-x, x] in the x-axis
+        pass.filter(*cloud);
 
-        pcl::visualization::CloudViewer viewer("Cloud Viewer");
-
-        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-        pcl::io::loadPCDFile ("data_collect_22Mar/record_"+ std::to_string(t)+".pcd", *cloud);
-
-        viewer.showCloud(cloud);
-
-        // viewer.runOnVisualizationThreadOnce (viewerOneOff);
-
-        viewer.runOnVisualizationThread (viewerPsycho);
-
-        while (!viewer.wasStopped ())
-        {
-            //you can also do cool processing here
-            //FIXME: Note that this is running in a separate thread from viewerPsycho
-            //and you should guard against race conditions yourself...
-            
-
-        
-
-            // if (t<100) {
-            //     pcl::io::loadPCDFile ("filtered_data/record_"+ std::to_string(t)+".pcd", *cloud);
-            //     viewer.showCloud(cloud);
-            //     t++;
-            // } else {
-            //     // t = 1;
-
-            //     break;
-            // }
-
-            user_data++;
-            
-        }
+        pass.setInputCloud(cloud);
+        pass.setFilterFieldName("y");
+        pass.setFilterLimits(-std::abs(std::stof(argv[2])), std::abs(std::stof(argv[2]))); // clamp points to the range of [-y, y] in the y-axis
+        pass.filter(*cloud);
 
 
-    // }
-    return 0;
+       // Apply seg
+       pcl::PointCloud <pcl::PointXYZRGB>::Ptr colored_cloud  = region_seg(cloud);
+
+       
+        // The pointcloud
+        viewer.addPointCloud<pcl::PointXYZRGB> (colored_cloud, "Nube cargada");
+        viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "Nube cargada");         
+
+        viewer.spinOnce(100);
+     }
 }
